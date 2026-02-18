b27146a3c1bca83bd0ed1ee96b461f1c22f60780
shawal-mbalire <mbalireshawal@gmail.com>
2026-02-18 10:00:59 +0300

    updated opencode to use open webui and added open webui and ollama docker composee yaml

--- dot-config/nvim/lua/plugins/core.lua
+++ dot-config/nvim/lua/plugins/core.lua
@@ -24,4 +24,17 @@ return {
       end,
     },
   },
+  {
+    "nvim-telescope/telescope.nvim",
+    opts = {
+      defaults = {
+        -- ...
+      },
+      pickers = {
+        find_files = {
+          hidden = true,
+        },
+      },
+    },
+  },
 }
