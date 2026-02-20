7beadb43d1987bc46ae9384866af7ca823f3c5ed
shawal-mbalire <mbalireshawal@gmail.com>
2026-02-20 12:32:17 +0300

    updated nvim

---
+++ dot-config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,11 @@
+return {
+  "folke/snacks.nvim",
+  opts = {
+    explorer = {
+      git = {
+        ignore = false, -- Show gitignored files
+      },
+      hidden = true, -- Show hidden files
+    },
+  },
+}
